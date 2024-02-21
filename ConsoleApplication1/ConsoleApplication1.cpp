4630eac3f48ae22e2d081a5106b5efab176d6775
dashapukhlenko <dpukhlenko@mail.ru>
2024-02-21 14:31:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+    int a, b, c;
+    cout << "Введите a, b: ";
+    cin >> a >> b;
+    c = a + c;
+    cout << c;
+}
+
+
